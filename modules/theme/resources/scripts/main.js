a012def14b3ff042f4497872d11477275160de9e
Sebastiaan Luca <hello@sebastiaanluca.com>
2016-10-25 22:12:11 +0200

    Set up Vue with datepicker

--- modules/theme/resources/scripts/main.js
+++ modules/theme/resources/scripts/main.js
@@ -5,18 +5,28 @@ import 'bootstrap-datepicker'
 
 //
 
-// TODO: recreate as a Vue component
-$('input.input-date').each(function () {
-    const locale = $(this).data('locale')
-    
-    $(this).datepicker({
-        language: locale,
-        format: $(this).data('date-format'),
-        weekStart: locale == 'en' ? 0 : 1,
-        maxViewMode: 1,
-        todayBtn: 'linked',
-        orientation: 'bottom auto',
-        autoclose: true,
-        todayHighlight: true,
-    });
+new Vue({
+    created: function () {
+        console.log('Vue ready!')
+        
+        this.initDateInputFields()
+    },
+    methods: {
+        initDateInputFields: function () {
+            $('input.input-date').each(function () {
+                const locale = $(this).data('locale')
+                
+                $(this).datepicker({
+                    language: locale,
+                    format: $(this).data('date-format'),
+                    weekStart: locale == 'en' ? 0 : 1,
+                    maxViewMode: 1,
+                    todayBtn: 'linked',
+                    orientation: 'bottom auto',
+                    autoclose: true,
+                    todayHighlight: true,
+                });
+            })
+        }
+    }
 })
